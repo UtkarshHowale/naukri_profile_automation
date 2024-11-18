29471ff8bd6711cd13be2b9ace97e5624c96e699
Utkarsh Howale <utkarshhowale16@gmail.com>
2024-11-18 13:25:57 +0530

    pushing updated changes

--- src/test/java/Tests/ProfileUpdateTestCases.java
+++ src/test/java/Tests/ProfileUpdateTestCases.java
@@ -38,19 +38,39 @@ public class ProfileUpdateTestCases extends BasePage {
 		closeConfiguration();
 	}
 
-	@Test
-	public void updateProfileTest() {
+	@Test(priority = 1)
+	public void updateProfileTest1() {
 
 		System.out.println("Login into the naukari profile with entring username & password");
-		dashboardPage = homePage.loginToNaukariProfile(prop.getProperty("Username"), prop.getProperty("Password"));
+		dashboardPage = homePage.loginToNaukariProfile(prop.getProperty("Username1"), prop.getProperty("Password1"));
 		System.out.println("Verfiy correct user is logged in.");
 		System.out.println("Sucessfully logged in with the correct user.");
-		assertEquals(dashboardPage.getProfileName(), prop.getProperty("ProfileName"),
+		assertEquals(dashboardPage.getProfileName(), prop.getProperty("ProfileName1"),
 				"Profile title not matching as expected...!!!");
 		System.out.println("Navigate to the profile page.");
 		profilePage = dashboardPage.navigateToProfilePage();
 		System.out.println("Update the resume headline.");
-		profilePage.updateResumeHeadline(prop.getProperty("ResumeHeadline"));
+		profilePage.updateResumeHeadline(prop.getProperty("ResumeHeadline1"));
+		System.out.println("Verify resume headline is updated.");
+		assertEquals(profilePage.getHeadlineUpdateSuccessMessage(), prop.getProperty("HeadlineUpdateSuccessMessage"),
+				"Resume headline is not updated...!!!");
+		System.out.println("Resume headline is updated successfully");
+
+	}
+	
+	@Test(priority = 2)
+	public void updateProfileTest2() {
+
+		System.out.println("Login into the naukari profile with entring username & password");
+		dashboardPage = homePage.loginToNaukariProfile(prop.getProperty("Username2"), prop.getProperty("Password2"));
+		System.out.println("Verfiy correct user is logged in.");
+		System.out.println("Sucessfully logged in with the correct user.");
+		assertEquals(dashboardPage.getProfileName(), prop.getProperty("ProfileName2"),
+				"Profile title not matching as expected...!!!");
+		System.out.println("Navigate to the profile page.");
+		profilePage = dashboardPage.navigateToProfilePage();
+		System.out.println("Update the resume headline.");
+		profilePage.updateResumeHeadline(prop.getProperty("ResumeHeadline2"));
 		System.out.println("Verify resume headline is updated.");
 		assertEquals(profilePage.getHeadlineUpdateSuccessMessage(), prop.getProperty("HeadlineUpdateSuccessMessage"),
 				"Resume headline is not updated...!!!");
